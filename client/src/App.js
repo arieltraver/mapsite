28f8566ec13c62334cad6430e7652e2a7b9a7118
arieltraver <arieltraver@users.noreply.github.com>
2023-05-01 14:10:25 -0400

    pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/home'
+import Post from './pages/post';
+import Create from './pages/create';
+import Edit from './pages/edit';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
@@ -10,6 +13,7 @@ import Container from 'react-bootstrap/Container';
 function App() {
   return (
     <>
+    
       <Navbar bg="dark" expand="lg" variant="dark">
         <Container>
           <Navbar.Brand href="/">Map Site</Navbar.Brand>
@@ -19,9 +23,14 @@ function App() {
           </Nav>
         </Container>
         </Navbar>
+
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/posts/:id"element={<Post/>}/>
+        <Route path="/posts/new"element={<Create/>}/>
+        <Route path="/posts/:id/edit"element={<Edit/>}/>
       </Routes>
+
     </>
   );
 }
